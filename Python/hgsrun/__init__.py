e180f6326d465c4c7debd1ea6a82726a407463ea
Andre R. Erler <A.R.Erler@gmail.com>
2016-07-31 14:59:43 -0400

    added project files and first Python package

---
+++ Python/hgsrun/__init__.py
@@ -0,0 +1,8 @@
+'''
+Created on 2016-07-31
+
+A package containing Python modules that help to run single or multiple HGS simulations 
+with different cliamte forcings.
+
+@author: Andre R. Erler, GPL v3
+'''
