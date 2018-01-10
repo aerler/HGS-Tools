cac2079281c8ccba0c0266225593773ac18930aa
Andre R. Erler <A.R.Erler@gmail.com>
2018-01-09 22:24:00 -0500

    some updates to EnKF run script

--- Scripts/run_enkf.sh
+++ Scripts/run_enkf.sh
@@ -11,7 +11,7 @@ N=$( sed -n '/total_number_of_time_steps/ s/^[[:space:]]*total_number_of_time_st
 echo "Total Number of Time-steps: $N"
 # clean a bit
 echo "Cleaning Run Directory"
-rm -rf proc_*/ out/ 
+rm -rf proc_*/ out/ backup.info enkf_*.log enkf_*.log.gz tmp.log
 mkdir out/
 echo
 # switch restart off
@@ -20,22 +20,48 @@ sed -i  '/restart_flag/ s/^\([[:space:]]*restart_flag[[:space:]]*=[[:space:]]\)*
 echo
 echo "Starting EnKF (first attempt)"
 echo $BIN
-$BIN
 echo
+$BIN > tmp.log 
+echo
+
+# check completion
+if [ -f backup.info ]; then
+  echo "  ..initialization successful!"
+else
+  echo
+  echo "  >>>  initialization failed  ---  aborting!!!"
+  mv tmp.log enkf_failed.log
+  echo "  (see log file 'enkf_failed.log' for details)" 
+  echo
+  exit 1
+fi # backup.info
+
+
 
 # read current time step
 CN=$( cat backup.info )
+LOG="enkf_${CN}.log"
+echo "Completed $CN times steps; compressing log file to '$LOG'"
+mv tmp.log $LOG
+gzip $LOG
+echo
 
 while [ $CN -lt $N ]; do
   echo
-  echo "EnKF assimilation step failed; attempting restart..."
+  echo "EnKF assimilation did not complete; attempting restart..."
   # set restart flag
   sed -i  '/restart_flag/ s/^\([[:space:]]*restart_flag[[:space:]]*=[[:space:]]\)*.*$/\11/g' EnKFparameters.ini
   # run again
   echo $BIN
-  $BIN
+  echo
+  $BIN > tmp.log
+  echo
   # read time step again
   CN=$( cat backup.info )
+  LOG="enkf_${CN}.log"
+  echo "Completed $CN times steps; compressing log file to '$LOG'"
+  mv tmp.log $LOG
+  gzip $LOG
   echo
 done # while CN < N
 
