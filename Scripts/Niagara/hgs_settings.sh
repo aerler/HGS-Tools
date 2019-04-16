9ee2a52f96cb3358c99d94f2a38f141d7a359d03
Andre Erler <aerler@atmosp.physics.utoronto.ca>
2019-04-16 17:37:28 -0400

    added fallback to load settings from home folder

--- Scripts/Niagara/hgs_settings.sh
+++ Scripts/Niagara/hgs_settings.sh
@@ -1,3 +1,10 @@
+# This is the settings/configuration file of the 'run_hgs_restart.sb' script
+# variable definitions in this file take precedence over default values that
+# are defined in teh caller script.
+# the default name of this script is 'hgs_settings.sh' and 'run_hgs_restart.sb'
+# will look for this script in its working directory first and then in the
+# $HOME directory of the user.
+# It is primarily intended to specify the path of the executables.
 GROKEXE="$HOME/hgs_bin/grok.x"
 HGSEXE="$HOME/hgs_bin/hgs.x"
 HSPEXE="$HOME/hgs_bin/hsplot.x"
